06710336fd6b68186835fbc60145167296bb4475
Alexandr Morozov <mopoz07@gmail.com>
2019-03-29 12:01:21 +0200

    finish block

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-
-const reducer = (state = 0, action) => {
-
-  switch(action.type) {
-    case 'INC':
-      return state + 1;
-
-    default:
-      return state;
-  }
-};
+import { Provider } from 'react-redux';
+import reducer from './reducer';
+import App from './components/app';
 
 const store = createStore(reducer);
-store.subscribe(() => {
-  console.log(store.getState());
-})
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
